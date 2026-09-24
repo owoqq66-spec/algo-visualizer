c851fc55aee7968fd25b163de573be9a4cd95876
owoqq66-spec <owoqq66@gmail.com>
2026-09-24 06:06:34 +0000

    feat: init vitepress with pages workflow

---
+++ .vitepress/config.mjs
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/algo-visualizer/',
+  title: "演算法視覺化教材",
+  description: "algo-visual",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+      text: '搜尋與指標',
+      items: [
+        { text: '二分搜尋法 (Binary Search)', link: '/binary-search' }
+      ]
+    },
+    {
+      text: '進階資料結構',
+      items: [
+        { text: '並查集 (Disjoint Set Union)', link: '/dsu' }
+      ]
+    },
+    {
+      text: '專案紀錄',
+      items: [
+        { text: '人機協作與 AI Debug 日誌', link: '/dev-log' }
+      ]
+    }
+  ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
